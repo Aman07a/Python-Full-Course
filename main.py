79e4a972ce9fb4561ef6b204f27f7d2556cfa3bc
Aman07a <aman16.aa17@gmail.com>
2024-03-14 11:26:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# variable = a reusable container for storing a value
-#            a variable behaves as if it were the value it contains
-
-# String Concatenation
-age = 21
-
-print("You are " + str(age) + " years old")
-
-# Separate Arguments
-print("You are", age, "years old")
-
-# F-Strings
-print(f"You are {age} years old")
+# # variable = a reusable container for storing a value
+# #            a variable behaves as if it were the value it contains
+#
+# # String Concatenation
+# age = 21
+#
+# print("You are " + str(age) + " years old")
+#
+# # Separate Arguments
+# print("You are", age, "years old")
+#
+# # F-Strings
+# print(f"You are {age} years old")
 
 # 4 Basic Data Types
 
