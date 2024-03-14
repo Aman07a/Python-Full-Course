06e23594c29ab3bbaa8e649bcb1e1b02f016c9c8
Aman07a <aman16.aa17@gmail.com>
2024-03-14 14:32:48 +0100

    Shopping Cart

--- UserInput.py
+++ UserInput.py
@@ -17,13 +17,23 @@
 # print(f"{noun} was {adjective2} and {verb}ing")
 # print(f"I was a {adjective3}")
 
-# Area Calculator
-length = float(input("Enter the length of a rectangle: "))
-width = float(input("Enter the width of a rectangle: "))
-height = float(input("Enter the height of a rectangle: "))
+# # Area Calculator
+# length = float(input("Enter the length of a rectangle: "))
+# width = float(input("Enter the width of a rectangle: "))
+# height = float(input("Enter the height of a rectangle: "))
+#
+# area = length * width
+# volume = length * width * height
+#
+# print(f"The area is: {area}cm^2")
+# print(f"The volume is: {volume}cm^3")
 
-area = length * width
-volume = length * width * height
+# Shopping Cart
+item = input("What item would you like  to buy?: ")
+price = float(input("What is the price?: "))
+quantity = int(input("How many would you like?: "))
 
-print(f"The area is: {area}cm^2")
-print(f"The volume is: {volume}cm^3")
+total = price * quantity
+
+print(f"You have bought {quantity} x {item}/s")
+print(f"Your total is: ${round(total, 2)}")
