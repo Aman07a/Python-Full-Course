c67918f52067634d7dfbd413443c24c6c3da4906
Aman07a <aman16.aa17@gmail.com>
2024-03-14 14:27:11 +0100

    Area Calculator

--- UserInput.py
+++ UserInput.py
@@ -5,14 +5,25 @@
 # print(f"Hello {name}")
 # print(f"You are {age} years old")
 
-# Mad Libs
-adjective1 = input("Enter an adjective: ")
-noun = input("Enter a noun: ")
-adjective2 = input("Enter an adjective: ")
-verb = input("Enter a verb: ")
-adjective3 = input("Enter an adjective: ")
+# # Mad Libs
+# adjective1 = input("Enter an adjective: ")
+# noun = input("Enter a noun: ")
+# adjective2 = input("Enter an adjective: ")
+# verb = input("Enter a verb: ")
+# adjective3 = input("Enter an adjective: ")
+#
+# print(f"Today I went to do a {adjective1} zoo.")
+# print(f"In an exhibit, I saw {noun}")
+# print(f"{noun} was {adjective2} and {verb}ing")
+# print(f"I was a {adjective3}")
 
-print(f"Today I went to do a {adjective1} zoo.")
-print(f"In an exhibit, I saw {noun}")
-print(f"{noun} was {adjective2} and {verb}ing")
-print(f"I was a {adjective3}")
+# Area Calculator
+length = float(input("Enter the length of a rectangle: "))
+width = float(input("Enter the width of a rectangle: "))
+height = float(input("Enter the height of a rectangle: "))
+
+area = length * width
+volume = length * width * height
+
+print(f"The area is: {area}cm^2")
+print(f"The volume is: {volume}cm^3")
