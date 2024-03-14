39cfcb6d724d12dca8272fca15dc9b418449c5ae
Aman07a <aman16.aa17@gmail.com>
2024-03-14 11:21:42 +0100

    Integers

--- main.py
+++ main.py
@@ -11,3 +11,21 @@ print("You are", age, "years old")
 
 # F-Strings
 print(f"You are {age} years old")
+
+# 4 Basic Data Types
+
+# INTEGER
+
+age = 21
+players = 2
+quantity = 5
+
+print(f"You are {age} years old")
+print(f"There are {players} players online")
+print(f"You would like to buy {quantity} items")
+
+# FLOAT
+
+# STRING
+
+# BOOLEAN
