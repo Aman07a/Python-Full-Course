5fc25c9281dbcd25fb715ff3805146d3473b9be3
Aman07a <aman16.aa17@gmail.com>
2024-03-14 11:39:54 +0100

    Strings

--- main.py
+++ main.py
@@ -14,26 +14,34 @@
 
 # 4 Basic Data Types
 
-# INTEGER
+# # INTEGER
+#
+# age = 21
+# players = 2
+# quantity = 5
+#
+# print(f"You are {age} years old")
+# print(f"There are {players} players online")
+# print(f"You would like to buy {quantity} items")
 
-age = 21
-players = 2
-quantity = 5
-
-print(f"You are {age} years old")
-print(f"There are {players} players online")
-print(f"You would like to buy {quantity} items")
-
-# FLOAT
-
-gpa = 3.2
-distance = 2.5
-price = 10.99
-
-print(f"Your gpa is {gpa}")
-print(f"You ran {distance} Km")
-print(f"The price is ${price}")
+# # FLOAT
+#
+# gpa = 3.2
+# distance = 2.5
+# price = 10.99
+#
+# print(f"Your gpa is {gpa}")
+# print(f"You ran {distance} Km")
+# print(f"The price is ${price}")
 
 # STRING
+
+name = "Bro"
+food = "pizza"
+email = "Bro123@gmail.com"
+
+print(f"Hello {name}")
+print(f"You like {food}")
+print(f"Your email is: {email}")
 
 # BOOLEAN
