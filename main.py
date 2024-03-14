b8703bc37e8c5d1dbb16d9b7c48dd2f63b680192
Aman07a <aman16.aa17@gmail.com>
2024-03-14 11:24:46 +0100

    Floats

--- main.py
+++ main.py
@@ -26,6 +26,14 @@ print(f"You would like to buy {quantity} items")
 
 # FLOAT
 
+gpa = 3.2
+distance = 2.5
+price = 10.99
+
+print(f"Your gpa is {gpa}")
+print(f"You ran {distance} Km")
+print(f"The price is ${price}")
+
 # STRING
 
 # BOOLEAN
