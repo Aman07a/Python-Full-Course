4905a1ddc7448589d74cf8fc001d039f1be352c9
Aman07a <aman16.aa17@gmail.com>
2024-03-14 12:11:04 +0100

    What is type casting?

---
+++ TypeCasting.py
@@ -0,0 +1,8 @@
+# typecasting = The process of converting a value of one data type to another
+#               (string, integer, float, boolean)
+#               Explicit vs Implicit
+
+name = "Bro"
+age = 21
+gpa = 1.9
+student = True
