6c58989855430bd0c338757be0f178130c7fb28b
Aman07a <aman16.aa17@gmail.com>
2024-03-14 13:49:46 +0100

    Mad Libs

--- UserInput.py
+++ UserInput.py
@@ -1,6 +1,18 @@
-# User Input
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))
-age = age + 1
-print(f"Hello {name}")
-print(f"You are {age} years old")
+# # User Input
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+# age = age + 1
+# print(f"Hello {name}")
+# print(f"You are {age} years old")
+
+# Mad Libs
+adjective1 = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+adjective2 = input("Enter an adjective: ")
+verb = input("Enter a verb: ")
+adjective3 = input("Enter an adjective: ")
+
+print(f"Today I went to do a {adjective1} zoo.")
+print(f"In an exhibit, I saw {noun}")
+print(f"{noun} was {adjective2} and {verb}ing")
+print(f"I was a {adjective3}")
